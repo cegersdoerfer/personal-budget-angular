011b2181cfbdb4bb15855aaae3b70b36dca9adb7
Chris Egersdoerfer <cegersdoerfer@gmail.com>
2023-09-15 01:13:09 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,31 +1,24 @@
 // Budget API
 
 const express = require('express');
-const cors = require('cors');
+const { get } = require('http');
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
-app.use(cors());
-
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
-
+function getData() {
+    try {
+        // Read the file synchronously and parse it to return the JSON data
+        const data = fs.readFileSync('data.json', 'utf-8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.log(err);
+        return null; // or some other error indication
+    }
+}
 
 app.get('/budget', (req, res) => {
+    const budget = getData();
     res.json(budget);
 });
 
